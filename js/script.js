0449c21b5d022c208976574a6ff57b1775fc6171
Heri Fiononana <HeryFiononana19@gmail.com>
2020-12-17 19:17:07 -0100

    initialization #master

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){ 							   
+
+	// radiys box	
+	$('.sb_menu li a, .ex_menu li a').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	$('.mainbar .spec a').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	$('.pagenavi a, .pagenavi .current').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	
+});	
